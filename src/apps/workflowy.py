0c34899cbb58544f8124b837496a5013ff6bbdcd
Danny Du <dannystdu@gmail.com>
2017-11-23 17:11:29 +0800

    added workflowy shortcuts
    Workflowy is a popular  outliner tool.

---
+++ src/apps/workflowy.py
@@ -0,0 +1,21 @@
+# coding: utf8
+#https://workflowy.com/
+shortcuts = {
+    "workflowy":{
+        "Zoom in": "⌘ + >",
+        "Zoom out": "⌘ + <",
+        "Expand/Collapse": "⌘ + Down/Up",
+        "Indent": "Tab",
+        "Outdent": "Shift + Tab",
+        "Move": "⌘ + Shift + Up/Down",
+        "Add a note": "Shift + Enter",
+        "Complete": "⌘ + Enter",
+        "Delete": "⌘ + Shift + Delete",
+        "Search": "ESC",
+        "Show/hide completed": "⌘ + O",
+        "Star page": "⌘ + Shift + ∗",
+        "Switch between pages": "⌘(hold) + ;",
+        "Bold / italic / underline": "⌘ + B/I/U",
+        "Keyboard shortcuts": "⌘ + ?"
+    }
+}
